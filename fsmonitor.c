392d797e2e60d1e6513a94783e248a3b57eb3c0f
Kevin Willford <kewillf@microsoft.com>
2019-10-31 11:00:04 -0600

    fsmonitor: do not check fsmonitor_dirty bits against cache entries on split index
    The fsmonitor_dirty bits are set based on all the entries of both the index
    and the base/shared index, where this check does not include the entries in
    the base/shared index.  This means the fsmonitor_dirty will almost always
    have more entries and hit this BUG.
    
    At read time the fsmonitor_dirty bits are loaded and checked before the
    base/shared index is read and entries from it are added to the cache.
    During write the fsmonitor_dirty bitmap is built based on all the entries
    of the in memory entries and not just the change index.
    
    Since either making the fsmonitor_dirty bitmap to only have entries for
    the one index or making sure when the check on the number of entries is
    taking into account both indexes is a more extensive change, skip the
    check on the number of entries when using a split index and fsmonitor.

--- fsmonitor.c
+++ fsmonitor.c
@@ -55,9 +55,10 @@ int read_fsmonitor_extension(struct index_state *istate, const void *data,
 	}
 	istate->fsmonitor_dirty = fsmonitor_dirty;
 
-	if (istate->fsmonitor_dirty->bit_size > istate->cache_nr)
+	if (!istate->split_index && istate->fsmonitor_dirty->bit_size > istate->cache_nr)
 		BUG("fsmonitor_dirty has more entries than the index (%"PRIuMAX" > %"PRIuMAX")",
 		    (uintmax_t)istate->fsmonitor_dirty->bit_size, (uintmax_t)istate->cache_nr);
+
 
 	trace_printf_key(&trace_fsmonitor, "read fsmonitor extension successful");
 	return 0;
@@ -83,7 +84,7 @@ void write_fsmonitor_extension(struct strbuf *sb, struct index_state *istate)
 	uint32_t ewah_size = 0;
 	int fixup = 0;
 
-	if (istate->fsmonitor_dirty->bit_size > istate->cache_nr)
+	if (!istate->split_index && istate->fsmonitor_dirty->bit_size > istate->cache_nr)
 		BUG("fsmonitor_dirty has more entries than the index (%"PRIuMAX" > %"PRIuMAX")",
 		    (uintmax_t)istate->fsmonitor_dirty->bit_size, (uintmax_t)istate->cache_nr);
 
