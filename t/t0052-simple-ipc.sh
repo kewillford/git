775e3b0a3d6b7b653950eb6fde6b0894980465eb
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-01-20 22:00:16 +0100

    simple-ipc: allow querying whether a given IPC channel is active
    prevent multiple listeners associated with the same path
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t0052-simple-ipc.sh
+++ t/t0052-simple-ipc.sh
@@ -11,11 +11,15 @@ test_lazy_prereq SUPPORTS_SIMPLE_IPC '
 test_expect_success SUPPORTS_SIMPLE_IPC 'simple command server' '
 	{ test-tool simple-ipc daemon & } &&
 
+	test_must_fail test-tool simple-ipc daemon &&
+	test-tool simple-ipc is-active &&
+
 	test-tool simple-ipc send ping >actual &&
 	echo pong >expect &&
 	test_cmp expect actual &&
 
 	test-tool simple-ipc send quit &&
+	test_must_fail test-tool simple-ipc is-active &&
 	test_must_fail test-tool simple-ipc send ping
 '
 
