fee7f2e0a562bf48723f86af6cb704c6003e65dd
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-01-20 23:58:28 +0100

    fsmonitor--daemon: add a command mode to stop a running daemon
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/t7527-internal-fsmonitor.sh
+++ t/t7527-internal-fsmonitor.sh
@@ -9,4 +9,18 @@ git fsmonitor--daemon --is-supported || {
 	test_done
 }
 
+test_expect_success 'can start and stop the daemon' '
+	test_when_finished \
+		"test_might_fail git -C test fsmonitor--daemon --stop" &&
+	git init test &&
+	(
+		cd test &&
+		: start the daemon implicitly by querying it &&
+		git fsmonitor--daemon --query 1 0 >actual &&
+		git fsmonitor--daemon --is-running &&
+		printf / >expect &&
+		test_cmp expect actual
+	)
+'
+
 test_done
