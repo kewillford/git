080e01acc33c37d435389ae58919f364ec7ea10f
Kevin Willford <Kevin.Willford@microsoft.com>
2020-01-31 10:28:20 -0700

    fsmonitor: add more test cases for internal monitor
    Signed-off-by: Kevin Willford <Kevin.Willford@microsoft.com>

--- t/t7527-internal-fsmonitor.sh
+++ t/t7527-internal-fsmonitor.sh
@@ -26,20 +26,36 @@ test_expect_success 'can start and stop the daemon' '
 		printf / >expect &&
 		test_cmp expect actual
 	) &&
-	sleep 0 &&
 	rm -rf test/.git &&
 	test_must_fail git -C test fsmonitor--daemon --is-running
 '
 
+# Note, after "git reset --hard HEAD" no extensions exist other than 'TREE'
+# "git update-index --fsmonitor" can be used to get the extension written
+# before testing the results.
+
+clean_repo () {
+	git reset --hard HEAD &&
+	git clean -fd &&
+	sleep 1 &&
+	>.git/trace
+}
+
 test_expect_success 'setup' '
 	: >tracked &&
 	: >modified &&
+	: >delete &&
+	: >rename &&
 	mkdir dir1 &&
 	: >dir1/tracked &&
 	: >dir1/modified &&
+	: >dir1/delete &&
+	: >dir1/rename &&
 	mkdir dir2 &&
 	: >dir2/tracked &&
 	: >dir2/modified &&
+	: >dir2/delete &&
+	: >dir2/rename &&
 	git -c core.fsmonitor= add . &&
 	test_tick &&
 	git -c core.fsmonitor= commit -m initial &&
@@ -59,14 +75,58 @@ test_expect_success 'setup' '
 	test_must_fail git fsmonitor--daemon --is-running
 '
 
+edit_files() {
+	echo 1 >modified
+	echo 2 >dir1/modified
+	echo 3 >dir2/modified
+	>dir1/untracked
+}
+
+delete_files() {
+	rm -f delete
+	rm -f dir1/delete
+	rm -f dir2/delete
+}
+
+rename_files() {
+	mv rename renamed
+	mv dir1/rename dir1/renamed
+	mv dir2/rename dir2/renamed
+}
+
 test_expect_success 'internal fsmonitor works' '
 	GIT_INDEX=.git/fresh-index git read-tree master &&
 	GIT_INDEX=.git/fresh-index git -c core.fsmonitor= status >expect &&
 	GIT_TRACE2_EVENT="$PWD/.git/trace" git status >actual &&
+	git fsmonitor--daemon --is-running &&
+	test_cmp expect actual
+'
+
+test_expect_success 'edit some files' '
+	clean_repo &&
+	edit_files &&
+	GIT_INDEX=.git/fresh-index git read-tree master &&
+	GIT_INDEX=.git/fresh-index git -c core.fsmonitor= status >expect &&
+	GIT_TRACE2_EVENT="$PWD/.git/trace" git status >actual &&
 	test_cmp expect actual &&
-	! grep yep .git/trace &&
-	>yep &&
-	grep yep .git/trace
+	sleep 1 &&
+	grep :\"dir1/modified\" .git/trace &&
+	grep :\"dir2/modified\" .git/trace &&
+	grep :\"modified\" .git/trace &&
+	grep :\"dir1/untracked\" .git/trace
+'
+
+test_expect_success 'delete some files' '
+	clean_repo &&
+	delete_files &&
+	GIT_INDEX=.git/fresh-index git read-tree master &&
+	GIT_INDEX=.git/fresh-index git -c core.fsmonitor= status >expect &&
+	GIT_TRACE2_EVENT="$PWD/.git/trace" git status >actual &&
+	test_cmp expect actual &&
+	sleep 1 &&
+	grep :\"dir1/delete\" .git/trace &&
+	grep :\"dir2/delete\" .git/trace &&
+	grep :\"delete\" .git/trace
 '
 
 test_expect_success 'can stop internal fsmonitor' '
@@ -74,6 +134,7 @@ test_expect_success 'can stop internal fsmonitor' '
 	then
 		git fsmonitor--daemon --stop
 	fi &&
+	sleep 1 &&
 	test_must_fail git fsmonitor--daemon --is-running
 '
 
